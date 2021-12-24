94fa376d4f5718d9884852770b40cc565dadbb4f
Luxmi Gupta <73395071+luxmi1799@users.noreply.github.com>
2021-12-24 18:10:18 +0530

    again commit

--- app/src/main/java/com/example/mapproject/Mainpage.java
+++ app/src/main/java/com/example/mapproject/Mainpage.java
@@ -1,14 +1,54 @@
 package com.example.mapproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class Mainpage extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#b03224")));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.search) {
+            Toast.makeText(getApplicationContext(), "Search", Toast.LENGTH_LONG).show();
+            return true;
+        } else if(id == R.id.map) {
+            Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
